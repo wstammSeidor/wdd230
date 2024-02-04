7ca731d52748c611e581ae3d618ca644c2655368
wstammSeidor <wstamm@seidoranalytics.com>
2024-02-04 17:19:56 -0300

    chammber week03

--- chamber/scripts/functions.js
+++ chamber/scripts/functions.js
@@ -29,6 +29,41 @@ try {
 
 
 
+    //counting visitors
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const currentDate = new Date();
+    
+        const lastVisitDate = localStorage.getItem('lastVisit');
+    
+        if (!lastVisitDate) {
+            displayMessage("Welcome! Let us know if you have any questions.");
+        } else {
+            const timeDifference = currentDate - new Date(lastVisitDate);
+    
+            const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    
+            if (daysDifference < 1) {
+                displayMessage("Back so soon! Awesome!");
+            } else {
+                const message = (daysDifference === 1) ? "day" : "days";
+                displayMessage(`You last visited ${daysDifference} ${message} ago.`);
+            }
+        }
+    
+        localStorage.setItem('lastVisit', currentDate.toISOString());
+    });
+    
+    function displayMessage(message) {
+        const messageFooter = document.getElementById('footer');
+        const messageElement = document.createElement('h2');
+        messageElement.textContent = message;
+        messageFooter.appendChild(messageElement);
+    }
+    
+
+
+
   } catch (error) {
     console.error("Error in functions.js:", error);
   }
